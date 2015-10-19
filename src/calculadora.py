d6f49ebe78c1433d533aae2734d4307aca311041
BRIAN-FERNANDEZ <BRIAN-FERNANDEZ@BRIAN>
2015-10-19 17:40:31 -0500

    ejercicios phyton

---
+++ src/calculadora.py
@@ -0,0 +1,36 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+#Crear una calculadora
+
+a=float(input('Ingrese primera variable'))
+b=float(input('Ingrese segunda variable'))
+print 'Seleccione una de las opciones que desee realizar'
+print '1.-Sumar las variables'
+print '2.-Restar las variables'
+print '3.-Multiplicar las variables'
+print '4.-Dividir las variables'
+print '5.-Potencia de la primera varible elevada a la segunda variable'
+c=input('Ingrese opcion a realizar')
+if c==1:
+    d=a+b
+    print d
+else:
+    if c==2:
+        d=a-b
+        print d
+    else:
+        if c==3:
+            d=a*b
+            print d
+        else:
+            if c==4:
+                d=a/b
+                print d
+            else:
+                if c==5:
+                    d=a**b
+                    print d
+                else:
+                    print 'Ingrese una de las opciones validas'
